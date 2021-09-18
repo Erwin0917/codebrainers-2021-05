d41f8fccc9f1294f80e8c1e3bbf5503b45abf1a0
Emilia Prusevicius <eprusevicius@gmail.com>
2021-09-18 16:54:42 +0200

    exercise-04 introduction to filter, map, reduce

--- scripts/index.js
+++ scripts/index.js
@@ -1,83 +1,19 @@
-const ticket = [2, 1, 3, 4, 5, 6];
+const arr = ['Ala', 'Kasia', 'Szymek', 'Tomek'];
 
-let lottoNumbers = [];
-let numberDrawn = [];
+function mapping(name, index) {
+console.log(name, index)
+}
 
-function populateArray(){
-    for(let i = 1; i < 50; i++) {
-       lottoNumbers.push(i);
+const newArr = arr.map(mapping);
+// arr.forEach(mapping)
+
+function filter(name) {
+    if(name === 'Szymek') {
+        return true;
     }
-}
+    return false;
+};
 
-function getRandomFromLottoNumber(arr) {
-    const number = Math.floor(Math.random() * arr.length) + 1;
-    // console.log('getRandomFromLottoNumber', number);
-    return number;
-}
+const newFilteredArr = arr.filter(filter);
 
-function pickNumber() {
-    const x = getRandomFromLottoNumber(lottoNumbers);
-    const index = lottoNumbers.indexOf(x);
-    lottoNumbers.splice(index,1);
-    return x;
-}
-
-function lottery() {
-    populateArray();
-    // console.log(`Lotto - ${lottoNumbers}`);
-    for (let i = 0; i < 6; i++) {
-        const numberPicked = pickNumber();
-        numberDrawn.push(numberPicked);
-        // console.log(`picked ${numberPicked}`);
-    }
-}
-
-function compareArraysOfNumbers(array1, array2){
-    if (array1.length !== array2.length){
-        // console.log('wrong array length')
-        return false;
-    }
-    for(let i = 0; i < array1.length; i++){
-        if (array1[i] !== array2[i]){
-            return false;
-        }
-    }
-    return true;
-
-}
-function sortArray(itemA, itemB){
-    // if (itemA < itemB){
-    //     return -1;
-    // } else if (itemA > itemB){
-    //     return 1;
-    // } else{
-    //     return 0;
-    // }
-    return itemA - itemB;
-}
-
-function checkTicket(){
-    ticket.sort(sortArray);
-    numberDrawn.sort(sortArray);
-    if (compareArraysOfNumbers(ticket, numberDrawn)){
-        return false;
-    }
-    return true;
-}
-
-let numberOfLotteries = 0;
-do {
-    lottery();
-    console.log('lottoNumbers', lottoNumbers);
-    console.log('numberDrawn', numberDrawn);
-    numberOfLotteries++;
-    lottoNumbers = [];
-    numberDrawn = [];
-    // console.log(checkTicket());
-}
-while (checkTicket());
-
-alert(`Ticket won in ${numberOfLotteries} draw`);
-
-
-
+arr.reduce()
