85d2d09970eae4db0c5ef08e394c00b7c8d411bd
Elineusz <Elineusz@gmail.com>
2021-09-18 16:02:20 +0200

    exercise-03 lottery ticket check and array sorting/push/slice etc...

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+const ticket = [2, 1];
+
 const lottoNumbers = [];
 const numberDrawn = [];
 
@@ -13,7 +15,7 @@ function getRandomFromLottoNumber(arr) {
 }
 
 function generateRandomNumberFromArray(array){
-    return array[getRandomFromLottoNumber(array)]
+    return array[getRandomFromLottoNumber(array)];
 }
 function pickNumber() {
     const x = generateRandomNumberFromArray(lottoNumbers);
@@ -22,10 +24,62 @@ function pickNumber() {
     return x;
 }
 
-populateArray();
-for( let i = 0; i < 6; i++){
-    numberDrawn.push(pickNumber());
+
+function lottery() {
+
+    populateArray();
+    console.log(`Lotto - ${lottoNumbers}`);
+    for (let i = 0; i < 6; i++) {
+        const numberPicked = pickNumber();
+        numberDrawn.push(numberPicked);
+        console.log(`picked ${numberPicked}`);
+    }
+    console.log(`finish lottery`);
 }
+
+function compareArraysOfNumbers(array1, array2){
+    if (array1.length !== array2.length){
+        return false;
+    }
+    for(let i = 0; i < array1.length; i++){
+        if (array1[i] !== array2[i]){
+            return false;
+        }
+    }
+    return true;
+
+}
+function sortArray(itemA, itemB){
+    // if (itemA < itemB){
+    //     return -1;
+    // } else if (itemA > itemB){
+    //     return 1;
+    // } else{
+    //     return 0;
+    // }
+    return itemA - itemB;
+}
+
+function checkTicket(){
+    ticket.sort(sortArray);
+    numberDrawn.sort(sortArray);
+    if (compareArraysOfNumbers(ticket, numberDrawn)){
+        return false;
+    }
+    return true;
+}
+
+
+
+let numberOfLotteries = 0;
+do {
+    lottery();
+    numberOfLotteries++;
+    console.log(checkTicket());
+}
+while (checkTicket());
+
+alert(`Ticket won in ${numberOfLotteries} draw`);
 
 console.log('lottoNumbers', lottoNumbers);
 console.log('numberDrawn', numberDrawn);
