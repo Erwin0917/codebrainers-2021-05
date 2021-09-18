41ef25e6d710164da9520edd9fda2dc2955d89e4
Erwin Heba <erwin0917@gmail.com>
2021-09-18 10:14:12 +0200

    exercise - 02 Introduce for loop

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,18 @@
-const subs = parseInt(prompt('how many subscribers do you have?'));
+//Math.floor()
+//Math.ceil()
+//Math.round()
+for(let i = 0; i < 4; i++){
+    const firstNumber = Math.floor(Math.random() * 10) + 1;
+    const secondNumber = Math.floor(Math.random() * 10) + 1;
 
-if (isNaN(subs)) {
-    console.log('Please type a number');
-} else {
-    if (subs >= 100) {
-        console.log('Congratulations!');
-    } else {
-        console.log(`You need ${100 - subs} subscribers to get the Silver Button`);
-    };
-};
+    const userResult = parseInt(prompt(`How much is ${firstNumber} * ${secondNumber}`));
+    const result = firstNumber * secondNumber;
+
+    console.log('userResult', userResult, typeof userResult);
+    console.log('result', result, typeof result);
+    console.log(userResult === result);
+
+    if(userResult === result){
+        alert('Your answer is correct!');
+    }
+}
