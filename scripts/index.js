1678f0f9490dc884cff21dc23e2de91e3999c51b
Erwin Heba <erwin0917@gmail.com>
2021-09-18 17:45:17 +0200

    exercise - 03 Lotto upgrade, refactor names, closers and problem with numberDrawn

--- scripts/index.js
+++ scripts/index.js
@@ -1,83 +1,73 @@
-const ticket = [2, 1, 3, 4, 5, 6];
+const ticket = [11, 15, 30, 33, 41, 46];
 
-let lottoNumbers = [];
-let numberDrawn = [];
-
-function populateArray(){
-    for(let i = 1; i < 50; i++) {
-       lottoNumbers.push(i);
+function populateArray() {
+    const newArray = [];
+    for (let i = 1; i < 50; i++) {
+        newArray.push(i);
     }
+
+    return newArray;
 }
 
-function getRandomFromLottoNumber(arr) {
-    const number = Math.floor(Math.random() * arr.length) + 1;
-    // console.log('getRandomFromLottoNumber', number);
-    return number;
+function getRandomFrom1UpTo(topRange) {
+    return Math.floor(Math.random() * topRange) + 1;
 }
 
-function pickNumber() {
-    const x = getRandomFromLottoNumber(lottoNumbers);
-    const index = lottoNumbers.indexOf(x);
-    lottoNumbers.splice(index,1);
-    return x;
-}
-
-function lottery() {
-    populateArray();
-    // console.log(`Lotto - ${lottoNumbers}`);
-    for (let i = 0; i < 6; i++) {
-        const numberPicked = pickNumber();
-        numberDrawn.push(numberPicked);
-        // console.log(`picked ${numberPicked}`);
-    }
-}
-
-function compareArraysOfNumbers(array1, array2){
-    if (array1.length !== array2.length){
-        // console.log('wrong array length')
+function compareArraysOfNumbers(array1, array2) {
+    if (array1.length !== array2.length) {
         return false;
     }
-    for(let i = 0; i < array1.length; i++){
-        if (array1[i] !== array2[i]){
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
             return false;
         }
     }
     return true;
 
 }
-function sortArray(itemA, itemB){
-    // if (itemA < itemB){
-    //     return -1;
-    // } else if (itemA > itemB){
-    //     return 1;
-    // } else{
-    //     return 0;
-    // }
+
+function sortArray(itemA, itemB) {
     return itemA - itemB;
 }
 
-function checkTicket(){
+function pickNumber(lottoNumbers) {
+    const pickedNumber = getRandomFrom1UpTo(lottoNumbers.length);
+    const indexOfPickedNumber = lottoNumbers.indexOf(pickedNumber);
+    lottoNumbers.splice(indexOfPickedNumber, 1);
+    return pickedNumber;
+}
+
+function lottery() {
+    const lottoNumbers = populateArray();
+    const numberDrawn = [];
+
+    for (let i = 0; i < 6; i++) {
+        const pickedNumber = pickNumber(lottoNumbers);
+        numberDrawn.push(pickedNumber);
+    }
+
+    return numberDrawn;
+}
+
+function checkTicket(numberDrown) {
     ticket.sort(sortArray);
-    numberDrawn.sort(sortArray);
-    if (compareArraysOfNumbers(ticket, numberDrawn)){
+    numberDrown.sort(sortArray);
+    if (compareArraysOfNumbers(ticket, numberDrown)) {
         return false;
     }
     return true;
 }
 
+
 let numberOfLotteries = 0;
+let jackpot = false;
 do {
-    lottery();
-    console.log('lottoNumbers', lottoNumbers);
-    console.log('numberDrawn', numberDrawn);
+    const numberDrown = lottery();
+    jackpot = checkTicket(numberDrown);
     numberOfLotteries++;
-    lottoNumbers = [];
-    numberDrawn = [];
-    // console.log(checkTicket());
-}
-while (checkTicket());
 
-alert(`Ticket won in ${numberOfLotteries} draw`);
+} while (jackpot);
 
+console.log('How many tickets for 6 numbers:', numberOfLotteries);
 
 
