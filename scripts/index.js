0fde3fd20d1e0e489df248813b96aef940a8ae54
Aleksei Taranov <aleksei.taranov@gmail.com>
2021-09-18 14:57:06 +0200

    exercise-03 GetRandomNumberArray Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,19 @@
 const lottoNumbers = [];
+const numberDrawn = [];
 
 function populateArray(){
-    for(let i = 1; i < 50; i++)
-        lottoNumbers.push(i);
+    for(let i = 1; i < 50; i++) {
+       lottoNumbers.push(i);
+    }
 }
 
+function getRandomFromLottoNumber(arr) {
+    const number = Math.floor(Math.random() * arr.length) + 1;
+    return number;
+}
 
 function generateRandomNumberFromArray(array){
-    return array[Math.floor(Math.random() * array.length)]
+    return array[getRandomFromLottoNumber(array)]
 }
 function pickNumber() {
     const x = generateRandomNumberFromArray(lottoNumbers);
@@ -18,6 +24,9 @@ function pickNumber() {
 
 populateArray();
 for( let i = 0; i < 6; i++){
-    console.log(pickNumber());
+    numberDrawn.push(pickNumber());
 }
+
+console.log('lottoNumbers', lottoNumbers);
+console.log('numberDrawn', numberDrawn);
 
