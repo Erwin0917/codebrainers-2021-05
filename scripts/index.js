0744b5700060afde438d5f1605c6d217422266a1
Emilia Prusevicius <eprusevicius@gmail.com>
2021-09-18 12:12:24 +0200

    exercise-02 refactoring, DRY

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,37 @@
 //Math.floor()
 //Math.ceil()
 //Math.round()
+
+function getRandomNumberFrom1To10() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+function getNumberOfQuestions() {
+    const result = prompt(`How many questions you want to answer? `);
+    console.log(parseInt(result))
+    return result;
+}
+
+function cancelAction(value) {
+    return value === null;
+}
+
+function isNumberOfQuestionsCorrect(value) {
+    return isNaN(value) || value < 1;
+}
+
 let score = 0;
 let questionCount = 0
 do {
-    questionCount = parseInt(prompt(`How many questions you want to answer? `));
-} while(isNaN(questionCount));
+    questionCount = getNumberOfQuestions();
+    if(cancelAction(questionCount)) {
+        break;
+    }
+} while(isNumberOfQuestionsCorrect(questionCount));
 
 for(let i = 0; i < questionCount; i++){
-    const firstNumber = Math.floor(Math.random() * 10) + 1;
-    const secondNumber = Math.floor(Math.random() * 10) + 1;
+    const firstNumber = getRandomNumberFrom1To10();
+    const secondNumber = getRandomNumberFrom1To10();
 
     const userResult = parseInt(prompt(`How much is ${firstNumber} * ${secondNumber}`));
     const result = firstNumber * secondNumber;
