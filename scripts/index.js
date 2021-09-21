ca718fa7caf9dcf4d0f664853576ed77c695f233
Jurek <jurekkopacz@gmail.com>
2021-09-21 20:46:20 +0200

    Exercise-05 Getter and setter, improve setSpeed() method

--- scripts/index.js
+++ scripts/index.js
@@ -21,7 +21,14 @@ class Car {
     }
 
     setSpeed(speed) {
-
+        if (typeof speed !== 'number') {
+            console.log('Wrong value!!');
+        } else if (speed > this.maximumSpeed || speed < 0) {
+            console.log('Speed to high or <0 !!');
+        } else {
+            this.speed = speed;
+            return true;
+        }
     }
 
     accelerate(howMuch) {
@@ -37,6 +44,12 @@ class Car {
 
 const newCar = new Car(200, 6);
 // const car2 = new Car(230, 4, 'red');
+
+newCar.setSpeed(210);
+newCar.setSpeed(260);
+newCar.setSpeed('qwe');
+console.log(newCar.setSpeed(-210));
+
 
 
 
