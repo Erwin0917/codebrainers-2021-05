69e147287e0ff84428c2e469e6d3c707f6d51fbe
Emilia Prusevicius <eprusevicius@gmail.com>
2021-09-21 18:09:34 +0200

    exercise-04 homework arrays

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,44 @@
-const arr = ['Ala', 'Kasia', 'Szymek', 'Tomek'];
+// ## Exercise 04
+//
+// ### TASK 1
+//
+// Write a function which takes an array of names as an argument and filters out names longer than 5 characters. Event better: names longer than a given number of characters.
+//
 
-function mapping(name, index) {
-console.log(name, index)
+const names = ['krystian', 'ala', 'barnaba', 'basia', 'jerzy', 'alicja', 'szymek'];
+
+function namesShorterThanGivenChars(charsLimit) {
+    const namesShorterThan = names.filter(name => name.length < charsLimit);
+    return namesShorterThan;
+    // return names.filter(name => name.length < charsLimit);
+};
+console.log(namesShorterThanGivenChars(5));
+
+//
+//
+// ### TASK 2
+//
+//
+// Create a mapping function which calculates the squere of the number of all elements of array, however. if given element of array is no a number, it should filter it out.
+
+const numbersAndNames = [3, 'krystian', 'ala', 2,  'barnaba', 5, 'basia', 10, 'jerzy', 'alicja', 'szymek', null, undefined];
+
+const numbersSquare = numbersAndNames.map(x => x**2);
+
+// function numbersOnly(value) {
+//     if (typeof(value) === 'number') {
+//         return value;
+//     }
+// };
+//
+// const numbers = numbersSquare.filter(numbersOnly);
+// console.log(numbers);
+
+const numbersOnly = value => {
+    if (typeof(value) === 'number') {
+        return value;
+    }
 }
 
-const newArr = arr.map(mapping);
-// arr.forEach(mapping)
-
-function filter(name) {
-    if(name === 'Szymek') {
-        return true;
-    }
-    return false;
-};
-
-const newFilteredArr = arr.filter(filter);
-
-arr.reduce()
+const numbers = numbersSquare.filter(numbersOnly);
+console.log(numbers);
