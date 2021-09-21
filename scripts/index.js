4d82a0f743443107aee5847cff435f7f93736732
Erwin Heba <erwin0917@gmail.com>
2021-09-21 19:10:47 +0200

    exercise - 05 Introducing to arrow function, Conditional operator

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,37 @@
-const arr = ['Ala', 'Kasia', 'Szymek', 'Tomek'];
 
-function mapping(name, index) {
-console.log(name, index)
-}
+const names = ['krystian', 'ala', 'barnaba', 'basia', 'jerzy', 'alicja', 'szymek'];
 
-const newArr = arr.map(mapping);
-// arr.forEach(mapping)
-
-function filter(name) {
-    if(name === 'Szymek') {
-        return true;
+function namesShorterThanGivenChars(callback) {
+    // const namesShorterThan = names.filter(name => {
+    //     return name.length < charsLimit;
+    // });
+    if (..) {
+        callback();
     }
-    return false;
+    return namesShorterThan;
+    // return names.filter(name => name.length < charsLimit);
 };
 
-const newFilteredArr = arr.filter(filter);
 
-arr.reduce()
+namesShorterThanGivenChars();
+const charFn = (numberOfChar => item => item.length > numberOfChar)(5);
+
+const charFn2 = numberOfChar => (item, index) => item.length > numberOfChar;
+
+const callFn = charFn2(5);
+// console.log(typeof  callFn);
+const nameLongerThan5 = names.filter(callFn);
+
+console.log(nameLongerThan5);
+
+const numbersOnly = value => typeof value === 'number' ? 'to jest number' : 'to nie jest number';
+
+const numbersAndNames = [3, 'krystian', 'ala', 2,  'barnaba', 5, 'basia', 10, 'jerzy', 'alicja', 'szymek', null, undefined];
+
+const arrayOfNumbers = numbersAndNames.filter(function (value, index) {
+    return numbersOnly(value);
+});
+
+const numbersSquare = arrayOfNumbers.map( item => item * item );
+
+// console.log(numbersSquare);
