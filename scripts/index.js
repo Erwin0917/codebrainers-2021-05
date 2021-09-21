932a10b22f75456ff35828bc55376bccd55003ce
Erwin Heba <erwin0917@gmail.com>
2021-09-21 20:08:27 +0200

    exercise - 05 POJO, class based object, init Car class

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,43 @@
+// POJO Plain Old Javascript Object
 
-const names = ['krystian', 'ala', 'barnaba', 'basia', 'jerzy', 'alicja', 'szymek'];
-
-function namesShorterThanGivenChars(callback) {
-    // const namesShorterThan = names.filter(name => {
-    //     return name.length < charsLimit;
-    // });
-    if (..) {
-        callback();
-    }
-    return namesShorterThan;
-    // return names.filter(name => name.length < charsLimit);
+const car = {
+    color: 'red',
+    maximumSpeed: 230,
+    numberOfWheels: 4
 };
 
 
-namesShorterThanGivenChars();
-const charFn = (numberOfChar => item => item.length > numberOfChar)(5);
+// Class-based object
 
-const charFn2 = numberOfChar => (item, index) => item.length > numberOfChar;
+class Car {
 
-const callFn = charFn2(5);
-// console.log(typeof  callFn);
-const nameLongerThan5 = names.filter(callFn);
+    constructor(speed, maximumSpeed, color = 'yellow') {
 
-console.log(nameLongerThan5);
+        if (typeof speed === 'number' && typeof maximumSpeed === 'number' && isNaN(speed) === false && isNaN(maximumSpeed) === false && typeof color === 'string') {
+            this.color = color;
+            this.maximumSpeed = maximumSpeed;
+            this.speed = speed;
+        }
+    }
 
-const numbersOnly = value => typeof value === 'number' ? 'to jest number' : 'to nie jest number';
+    setSpeed(speed) {
 
-const numbersAndNames = [3, 'krystian', 'ala', 2,  'barnaba', 5, 'basia', 10, 'jerzy', 'alicja', 'szymek', null, undefined];
+    }
 
-const arrayOfNumbers = numbersAndNames.filter(function (value, index) {
-    return numbersOnly(value);
-});
+    accelerate(howMuch) {
 
-const numbersSquare = arrayOfNumbers.map( item => item * item );
+    }
 
-// console.log(numbersSquare);
+    decelerate(howMuch) {
+
+    }
+
+
+}
+
+const newCar = new Car(200, 6);
+// const car2 = new Car(230, 4, 'red');
+
+
+
+
