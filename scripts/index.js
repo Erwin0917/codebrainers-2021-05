03c526b1ce4fada7371bd0a382e1ecb2b00a4d01
Emilia Prusevicius <eprusevicius@gmail.com>
2021-09-25 15:26:00 +0200

    exercise-06 added weapon generator

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+function getRandomNumberFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 class Person {
     constructor() {
         this.hitPoints = 0;
@@ -11,13 +15,14 @@ class Person {
 }
 
 class Weapon {
-    constructor(minDamage, maxDamage){
+    constructor(minDamage, maxDamage, reqStrength){
         this.minDamage = minDamage;
         this.maxDamage = maxDamage;
+        this.reqStrength = reqStrength;
     }
 
     getHitDamage() {
-        return Math.floor(Math.random() * (this.maxDamage - this.minDamage + 1) + this.minDamage);
+        return getRandomNumberFromRange(this.minDamage, this.maxDamage);
     }
 }
 
@@ -53,11 +58,9 @@ class Character extends Person {
     }
 
     setWeapon(weapon) {
-
-        if (weapon instanceof Weapon) {
+        if (weapon instanceof Weapon && this.strength >= weapon.reqStrength) {
             this.weapon = weapon;
         }
-
     }
 
     setHitPoints(hitpoints) {
@@ -77,18 +80,27 @@ class Villain extends Character {
     constructor() {
         super();
         this.hitPoints = 100;
-        this.strength = 10;
+        this.strength = 40;
     }
 }
 
+const weapons = [];
 
+function generateWeapon() {
+    return new Weapon(getRandomNumberFromRange(3, 10), getRandomNumberFromRange(40, 60), getRandomNumberFromRange(0, 50));
+};
 
+for (let i = 1; i <= 15; i++) {
+    weapons.push(generateWeapon());
+}
+
+console.log(weapons);
 
 const villain = new Villain();
 const hero = new Hero();
 
-const axe = new Weapon(7, 15);
-const knife = new Weapon(7, 10);
+const axe = new Weapon(7, 15, 5);
+const knife = new Weapon(7, 10, 10);
 
 //console.log(villain);
 // hero.attack(villain, 30);
@@ -97,25 +109,22 @@ villain.setWeapon(axe);
 hero.setWeapon(knife);
 
 function attack(attacker, target, attackerName, targetName){
-
-
     if (attacker.isAlive()) {
         console.log(attackerName, ' attacks: ');
         attacker.attack(target, 2);
-        console.log(targetName, ' after ', attackerName, '\'s attack: ', target.hitPoints);
+        console.log(`${targetName} after ${attackerName}'attack ${target.hitPoints}`);
         if(!target.isAlive()){
             console.log(targetName, 'is dead');
         }
     }
-
-
 }
+
 function duel(attacker, target, attackerName, targetName){
     attack(attacker, target, attackerName, targetName);
     attack(target, attacker, targetName, attackerName);
 }
 
-while (hero.isAlive() && villain.isAlive()) {
-    duel(hero, villain, 'Hero', 'Villain');
-}
+// while (hero.isAlive() && villain.isAlive()) {
+//     duel(hero, villain, 'Hero', 'Villain');
+// }
 
