1ca57e52c2e362c7b0bcba970d5e6f525810ed0e
Jakub Argasiński <jakub@argasinski.eu>
2021-10-14 20:53:44 +0200

    Exercise 10 - Extract Rectangle component code to a separate JS file.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,6 @@
 import React from 'react';
 import './App.css';
-
-function Rectangle(props) {
-    let i, j;
-
-    let rectangle = '';
-
-    rectangle += '+';
-    for (i = 0; i < props.a; i++) {
-        rectangle += '-';
-    }
-    rectangle += '+\n';
-
-    for (j = 0; j < props.b; j++) {
-        rectangle += '|';
-        for (i = 0; i < props.a; i++) {
-            rectangle += ' ';
-        }
-        rectangle += '|\n';
-    }
-
-    rectangle += '+';
-    for (i = 0; i < props.a; i++) {
-        rectangle += '-';
-    }
-    rectangle += '+\n';
-
-    return (
-        <div>
-            <pre className="rectangle">{rectangle}</pre>
-            <p>Area of rectangle of width {props.a} and height {props.b} is {props.a * props.b}.</p>
-        </div>
-    );
-}
+import Rectangle from "./Rectangle";
 
 function App() {
     return (
