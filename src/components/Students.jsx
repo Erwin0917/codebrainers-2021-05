cc08f2f4cf085c4fe71556cf38286d0a26e8361b
FranFamen <franciszek.saklawski@gmail.com>
2021-10-16 15:34:55 +0200

    exercise-11 Add state change in sortStudent, add button for sorting

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -6,10 +6,16 @@ class Students extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            averageAge: undefined
+            averageAge: undefined,
+            sortedStudents: []
         };
 
+
     }
+    componentDidMount() {
+        this.setState({sortedStudents: this.props.students})
+    }
+
     calculateAverageAge = () => {
         if (this.props.students.length === 0){
             return;
@@ -20,7 +26,7 @@ class Students extends React.Component {
         this.setState({averageAge: averageAge});
     }
 
-    render() {
+    sortStudent = () =>{
         const students = this.props.students;
         const sortedStudents = students.sort(function (studentA, studentB) {
             const fullName1 = studentA.fullName;
@@ -34,6 +40,10 @@ class Students extends React.Component {
             }
 
         });
+        this.setState({sortedStudents: sortedStudents});
+    }
+    render() {
+
         return (
             <React.Fragment>
                 <table className="studentsTable" cellPadding='0' cellSpacing='0'>
@@ -53,7 +63,7 @@ class Students extends React.Component {
                     <tbody>
 
                     {
-                        sortedStudents.map(
+                        this.state.sortedStudents.map(
                             student => <Student student={student}/>
                         )
                     }
@@ -61,6 +71,7 @@ class Students extends React.Component {
 
                 </table>
                 <p><button onClick={this.calculateAverageAge} className="calculate-age">Calculate average age</button></p>
+                <p><button onClick={this.sortStudent} className="calculate-age">Sort students</button></p>
                 <div>
                     <p>Average age: {this.state.averageAge}</p>
 
