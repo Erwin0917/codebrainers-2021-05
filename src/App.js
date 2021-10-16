aec317cac8c7f6d04dbbdeaa70d3acbac054deed
Jurek <jurekkopacz@gmail.com>
2021-10-16 10:44:52 +0200

    exercise-11 render basic list of students

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import codeBrainersStudents from './models/Students';
+import Student from "./components/Student";
 
 function App() {
     return (
         <div>
             <div>
+                {
+                    codeBrainersStudents.map(
+                        student => <Student student={student} />
+                    )
+                }
             </div>
         </div>
     );
--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Student(props) {
-    return 'Student';
+    return <div>{props.student.fullName}</div>;
 }
 
 export default Student;
