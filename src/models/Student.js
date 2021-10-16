f27760e6f05f103f01fdd93d2f50fcb0db28dfdc
FranFamen <franciszek.saklawski@gmail.com>
2021-10-16 09:53:10 +0200

    exercise-11 Create Student class, populate students table

---
+++ src/models/Student.js
@@ -0,0 +1,13 @@
+
+
+
+class Student{
+    constructor(fullName = '', age = 0, lessonCount = 0) {
+        this.fullName = fullName;
+        this.age = age;
+        this.lessonCount = lessonCount;
+    }
+}
+
+
+export default Student;
---
+++ src/models/Students.js
@@ -0,0 +1,12 @@
+import Student from "./Student";
+
+const codeBrainersStudents = [
+    new Student('Franciszek', 26, 12),
+    new Student('Jurek', 27, 4),
+    new Student('Marcin', 28, 90),
+    new Student('Aleksei', 29, 7),
+    new Student('Emilia', 57, 1),
+    new Student('Jakub', 13, 0)
+];
+export default codeBrainersStudents;
+
