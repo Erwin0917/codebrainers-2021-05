1c428ac339a277314cac3e3a42acf7d14deb0b45
Jakub Argasiński <jakub@argasinski.eu>
2021-11-20 10:30:12 +0100

    Exercise-17 Authentication, validation, refactoring

--- src/components/authenticated/AuthenticatedNavbar.jsx
+++ src/components/authenticated/AuthenticatedNavbar.jsx
@@ -1,59 +1,56 @@
 import {
-  Collapse,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Nav,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  UncontrolledDropdown,
+    Collapse,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    UncontrolledDropdown,
 } from "reactstrap";
 import AuthenticatedNavbarItem from "components/navbar/AuthenticatedNavbarItem";
 import Routes from "constants/Routes";
-import { faCog } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faCog} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
 import navbarItems from "components/navbar/constants/NavbarItems";
 import {useState} from "react";
 
-function PlantasticNavbar () {
-  const [ isOpen, setIsOpen ] = useState(false);
-  function toggle () {
-    setIsOpen(!isOpen);
-  }
-      return (
+function PlantasticNavbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
         <Navbar color="dark" dark expand="md" className="mb-4">
-          <NavbarBrand href={Routes.ROOT}>Plantastic</NavbarBrand>
-          <NavbarToggler onClick={() => toggle()} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="mr-auto" navbar>
-              {navbarItems.map((navbarItem) => (
-                  <AuthenticatedNavbarItem
-                      path={navbarItem.path}
-                      icon={navbarItem.icon}
-                      name={navbarItem.name}
-                      key={navbarItem.path}
-                  />
-              ))}
-            </Nav>
-            <Nav navbar>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  <FontAwesomeIcon icon={faCog} /> Account
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Preferences…</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Logout</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
+            <NavbarBrand href={Routes.ROOT}>Plantastic</NavbarBrand>
+            <NavbarToggler onClick={() => setIsOpen(!isOpen)}/>
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                    {navbarItems.map((navbarItem) => (
+                        <AuthenticatedNavbarItem
+                            path={navbarItem.path}
+                            icon={navbarItem.icon}
+                            name={navbarItem.name}
+                            key={navbarItem.path}
+                        />
+                    ))}
+                </Nav>
+                <Nav navbar>
+                    <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            <FontAwesomeIcon icon={faCog}/> Account
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                            <DropdownItem>Preferences…</DropdownItem>
+                            <DropdownItem divider/>
+                            <DropdownItem>Logout</DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>
+                </Nav>
+            </Collapse>
         </Navbar>
     );
 }
-
 
 
 export default PlantasticNavbar;
